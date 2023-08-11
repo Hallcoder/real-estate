f9ba6cccec03867bf71341d2ec370c60da7a495e
Hallcoder <hallcoder25@gmail.com>
2023-08-11 11:48:58 +0200

    [feat]: overlay text

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -5,7 +5,7 @@ import img3 from '../assets/hero-bg1.jpg';
 const Categories = () => {
   const items = [
     { content: 'Appartments', cols: 2, image:img1}, // This item will span 2 columns
-    { content: 'Commercial Homes', cols: 1, image:img2 },
+    { content: 'Commercial Properties', cols: 1, image:img2 },
     { content: 'Plots of Land', cols: 1, image:img3 },
     { content: 'Residential Homes', cols: 2, image:img1 },
   ];
@@ -21,7 +21,7 @@ const Categories = () => {
         >
           <div className="relative cursor-pointer h-[60vh] m-2">
             <img src={item.image} alt="" className='object-fill transition-transform transform-gpu hover:scale-105 hover:shadow-lg hover:shadow-black shadow-gray-400 shadow-md rounded-[2%] h-full w-full'/>
-            <p className='absolute text-center w-full bottom-[10%]  text-4xl text-white'>{item.content}</p>
+            <p className='absolute w-2/6 bottom-[5%] left-[1%] font-semibold text-4xl text-white'>{item.content}</p>
           </div>
         </div>
       ))}
