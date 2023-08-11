74d99eb4130b39571673d27ca4a53582d2e26439
Hallcoder <hallcoder25@gmail.com>
2023-08-11 11:31:39 +0200

    visual updates

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -4,14 +4,14 @@ import img3 from '../assets/hero-bg1.jpg';
 
 const Categories = () => {
   const items = [
-    { content: 'Item 1', cols: 2, image:img1}, // This item will span 2 columns
-    { content: 'Item 2', cols: 1, image:img2 },
-    { content: 'Item 4', cols: 1, image:img3 },
-    { content: 'Item 2', cols: 2, image:img1 },
+    { content: 'Appartments', cols: 2, image:img1}, // This item will span 2 columns
+    { content: 'Commercial Homes', cols: 1, image:img2 },
+    { content: 'Plots of Land', cols: 1, image:img3 },
+    { content: 'Residential Homes', cols: 2, image:img1 },
   ];
 
   return (
-    <div className="grid grid-cols-3 gap-4 mt-20 mx-8 rounded-md">
+    <div className="grid grid-cols-3 gap-6 mt-20 mx-8 rounded-md">
       {items.map((item, index) => (
         <div
           key={index}
@@ -19,8 +19,9 @@ const Categories = () => {
             item.cols === 2 ? 'col-span-2' : 'col-span-1'
           }`}
         >
-          <div className="h-[60vh] m-2">
-            <img src={item.image} alt="" className='object-fill rounded-[20%] h-full w-full'/>
+          <div className="relative cursor-pointer h-[60vh] m-2">
+            <img src={item.image} alt="" className='object-fill transition-transform transform-gpu hover:scale-105 hover:shadow-lg hover:shadow-black shadow-gray-400 shadow-md rounded-[2%] h-full w-full'/>
+            <p className='absolute text-center w-full bottom-[10%]  text-4xl text-white'>{item.content}</p>
           </div>
         </div>
       ))}
